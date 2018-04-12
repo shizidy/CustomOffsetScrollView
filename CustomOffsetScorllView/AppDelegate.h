d77cba3ed9187459acc1908e98b8010ef36c2060
wdyzmx <wdyzmx@wdyzmxdemacbook-pro.local>
2018-04-12 17:39:09 +0800

    Initial Commit

---
+++ CustomOffsetScorllView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomOffsetScorllView
+//
+//  Created by wdyzmx on 2018/4/12.
+//  Copyright © 2018年 wdyzmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomOffsetScorllView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomOffsetScorllView
+//
+//  Created by wdyzmx on 2018/4/12.
+//  Copyright © 2018年 wdyzmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
